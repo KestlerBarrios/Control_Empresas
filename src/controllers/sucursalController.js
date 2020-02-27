2661c009c5511150a5765dcd5a20b2fe7d300f0a
KestlerBarrios <luiseduardo3213@gmail.com>
2020-02-26 23:27:04 -0600

    Agregar Sucursal

--- src/controllers/sucursalController.js
+++ src/controllers/sucursalController.js
@@ -1,6 +1,23 @@
 'use strict'
 
+const Empresa = require('../models/Empresa')
+const Sucursal = require('../models/Sucursal')
+
 function crearSucursal(req, res) {
+var sucursal = new Sucursal()
+var empresaId = req.params.id
+var params = req.body
+
+if(params.nombre){
+    sucursal.nombre = params.nombre
+    sucursal.empresa = req.empresa.sub
+    sucursal.save((err, sucursalGuardada) => {
+        if(err) return res.status(500).send({ message: 'Error en la peticion' })
+            if(!sucursal) return res.status(404).send({ message: 'Error al agregar la sucursal' })
+            return res.status(200).send({message: 'Sucursal Guardada', sucursal: sucursalGuardada})
+    })
+}
+
     
 }
 
